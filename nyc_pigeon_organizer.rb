4bc764c07eb06659766e8f9b24bb4142f718f351
Kimberly Huynh <huynh.h.kimberly@gmail.com>
2019-09-30 20:48:30 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,16 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  organized_data = {}
+  
+  data.each do |atribute,value|
+    value.each do |sub_atribute,pigeon_name|
+      pigeon_name.each do |name|
+        organized_data[name] = {} if !organized_data[name]
+        organized_data[name][atribute] =[] if !organized_data[name][atribute]
+        organized_data[name][atribute].push(sub_atribute.to_s)
+        
+      end
+    end
+  end
+
+  organized_data
 end
